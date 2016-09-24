4bc1e3061d7a38d95f592d4ab39063ebd955526d
Alejandro <Alejandro@JSC3000>
2016-09-24 11:23:14 +0300

    Add project files.

---
+++ Client/MainWindow.xaml.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Net;
+using System.Net.Sockets;
+namespace Client
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        TcpClient client;
+        
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void buttonSend_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                IPEndPoint ep = new IPEndPoint(IPAddress.Parse(textBoxIP.Text), Int32.Parse(textBoxPort.Text));
+
+                client = new TcpClient();
+                client.Connect(ep);
+
+                NetworkStream netStream = client.GetStream();
+                byte[] messageArray = Encoding.Unicode.GetBytes(textBoxMessage.Text);
+                netStream.Write(messageArray, 0, messageArray.Length);
+
+                client.Close();
+            }
+            catch(SocketException ex)
+            {
+                MessageBox.Show("Socket error: " + ex.Message, "Socket error!", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show("Error: " + ex.Message, "Error!", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+        }
+
+    }
+}
---
+++ Server/Form1.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Windows.Forms;
+using System.Net;
+using System.Net.Sockets;
+using System.IO;
+
+namespace Server
+{
+    public partial class Form1 : Form
+    {
+        TcpListener listener;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                listener = new TcpListener(IPAddress.Parse(textBox1.Text), Int32.Parse(textBox2.Text));
+                listener.Start();
+
+                Thread th = new Thread(threadRead);
+                th.IsBackground = true;
+                th.Start();
+            }
+            catch (SocketException ex)
+            {
+                MessageBox.Show("Socket error: " + ex.Message);
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show("Error: " + ex.Message);
+            }
+        }
+
+        void threadRead()
+        {
+            TcpClient cl = listener.AcceptTcpClient();
+
+            StreamReader sr = new StreamReader(cl.GetStream(), Encoding.Unicode);
+            String str = sr.ReadLine();
+
+            listBox1.Items.Add("Recieved from: " + cl.Client.RemoteEndPoint.ToString());
+            listBox1.Items.Add(str);
+            cl.Close();
+        }
+        private void Form1_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            if (listener != null)
+                listener.Stop();        
+        }
+    }
+}
